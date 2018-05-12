99995a6267f8c0e787626967442aab6e86d71f06
Damian <emer_smart17@outlook.com>
2018-05-11 21:16:23 -0500

    Vistas
    Vistas principales en proceso

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//Enrutamiento para pruebas 
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { PackagesComponent } from './components/packages/packages.component';
 import { PhotosComponent } from './components/photos/photos.component';
+import { HomeComponent } from './components/home/home.component';
 
+//Enrutamiento de prueba
+const appRoutes: Routes = [
+  {path: 'inicio', component: HomeComponent},
+  {path: 'contacto', component: ContactComponent},
+  {path: 'paquetes', component: PackagesComponent},
+  {path: 'fotos', component: PhotosComponent},
+  {path: '', redirectTo:'/inicio', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactComponent,
     PackagesComponent,
-    PhotosComponent
+    PhotosComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      {  enableTracing: true}
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
